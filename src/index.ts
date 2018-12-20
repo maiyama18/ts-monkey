b7f5d5350c913aa82953e65d2e0c466202d997ee
ymr-39 <myam0118@gmail.com>
2018-12-20 23:53:39 +0900

    export lex/parse/eval

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { Evaluator } from './evaluator/evaluator';
+import { Lexer } from './lexer/lexer';
+import { Program } from './node/node';
+import { Environment } from './object/environment';
+import { Parser } from './parser/parser';
+import { Token } from './token/token';
+
+export const lex = (input: string): Token[] => {
+    const lexer = new Lexer(input);
+
+    const result = [];
+    while (true) {
+        const token = lexer.nextToken();
+        result.push(token);
+        if (token.type === 'EOF') { break; }
+    }
+
+    return result;
+};
+
+export const parse = (input: string): Program => {
+    const lexer = new Lexer(input);
+    const parser = new Parser(lexer);
+
+    return parser.parseProgram();
+};
+
+export const evaluate = (input: string): string => {
+    const lexer = new Lexer(input);
+    const parser = new Parser(lexer);
+
+    const program = parser.parseProgram();
+
+    const evaluator = new Evaluator();
+    const env = new Environment();
+
+    return evaluator.eval(program, env).inspect();
+};
