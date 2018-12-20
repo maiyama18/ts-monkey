8554626062303b103245480975f7d97e7aaa159e
ymr-39 <myam0118@gmail.com>
2018-12-21 01:40:41 +0900

    change version

---
+++ src/repl/repl.ts
@@ -0,0 +1,36 @@
+import * as readline from 'readline';
+import { Evaluator } from '../evaluator/evaluator';
+import { Lexer } from '../lexer/lexer';
+import { Environment } from '../object/environment';
+import { Parser } from '../parser/parser';
+
+const PROMPT = '-> ';
+
+const env = new Environment();
+const evaluator = new Evaluator();
+
+const io = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const repl = () => {
+    io.question(PROMPT, (input) => {
+        const lexer = new Lexer(input);
+        const parser = new Parser(lexer);
+
+        try {
+            const program = parser.parseProgram();
+            const obj = evaluator.eval(program, env);
+            if (obj.objType !== 'NIL') {
+                console.log(obj.inspect());
+            }
+        } catch (err) {
+            console.error(err.message);
+        }
+
+        repl();
+    });
+};
+
+repl();
